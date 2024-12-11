9267b1526ee49fbfcbd752f6c0c0894d1478486b
YegoEDDY <35481971+YegoEDDY@users.noreply.github.com>
2024-12-11 09:21:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -10,14 +10,14 @@ SESSION_ID: process.env.SESSION_ID || "zJwQhKbD#1Z6muaYdmPhpkQOX-Lc884BNELjT8U1K
 MODE: process.env.MODE || "public",  // Add Your Bot Mode (groups/privet/public)
 PREFIX: process.env.PREFIX || ".",   // Add Your Custom Prefix 
 OWNER_REACT: process.env.OWNER_REACT || "true", //add true or false
-BOT_NAME: process.env.BOT_NAME || "𝙼𝙰𝙻𝚅𝙸𝙽 𝙼𝙳 v2",
+BOT_NAME: process.env.BOT_NAME || "Yego MD",
 ///=================When editing, do not accidentally delete anything !!CUSTOM-SETTING-CONFIG=====================
-AUTO_VOICE: process.env.AUTO_VOICE || "true", //add true or false
-AUTO_STICKER: process.env.AUTO_STICKER || "true", //add true or false
-AUTO_REPLY: process.env.AUTO_REPLY || "true", //add true or false
-ANTI_LINK: process.env.ANTI_LINK || "true", //add true or false
-ANTI_BAD: process.env.ANTI_BAD || "true", //add true or false
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true", //add true or false
+AUTO_VOICE: process.env.AUTO_VOICE || "false", //add true or false
+AUTO_STICKER: process.env.AUTO_STICKER || "false", //add true or false
+AUTO_REPLY: process.env.AUTO_REPLY || "false", //add true or false
+ANTI_LINK: process.env.ANTI_LINK || "false", //add true or false
+ANTI_BAD: process.env.ANTI_BAD || "false", //add true or false
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false", //add true or false
 AUTO_REACT: process.env.AUTO_REACT || "true", //add true or false
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", //add true or false
 ///=================When editing, do not accidentally delete anything. !!API-CONFIG=================================
